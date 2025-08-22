1e8439a6443a05e744ded2691ea85d3ad6baa81b
AxelS <72924802+SHOOTSTV@users.noreply.github.com>
2025-08-23 01:13:49 +0200

    feat: add random color  when hovering  squares with mouse

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const grid = document.querySelector(".grid");
-
 const defaultGridSize = 16;
 
-// create and styles squares in grid
+// create and styles squares o grid
 for (let i = 0; i < defaultGridSize * defaultGridSize; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
-  // gives each square an even space on the grid
-  square.style.flex = `1 0 ${100 / defaultGridSize}%`;
+  square.style.flex = `1 0 ${100 / defaultGridSize}%`; // gives each square an even space on the grid
   square.style.boxSizing = "border-box";
   square.style.border = "1px solid #ccc";
   grid.appendChild(square);
+  // on hover fill the square with a random rgb color
+  square.addEventListener(
+    "mouseenter",
+    (event) => {
+      event.target.style.backgroundColor = getRandomColor();
+    },
+    false
+  );
+}
+
+function getRandomColor() {
+  return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
 }
