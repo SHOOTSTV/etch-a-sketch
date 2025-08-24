0b7c1adcf6cf30f3f68543fb12f696edf5307d37
AxelS <72924802+SHOOTSTV@users.noreply.github.com>
2025-08-24 18:02:17 +0200

    feat: add a button to change the grid size and optimize code logic

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
-const grid = document.querySelector(".grid");
-const defaultGridSize = 16;
+const gridContainer = document.querySelector(".grid");
+const gridSizeButton = document.querySelector("#size-btn");
 
-// create and styles squares o grid
-for (let i = 0; i < defaultGridSize * defaultGridSize; i++) {
+const DEFAULT_GRID_SIZE = 16;
+let userGridSize = undefined;
+
+renderGrid();
+
+function renderGrid() {
+  // reset the grid before building it
+  gridContainer.innerHTML = "";
+  const gridSize = userGridSize || DEFAULT_GRID_SIZE;
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    createGridSquare(gridSize);
+  }
+}
+
+// on hover fill the square with a random rgb color
+gridContainer.addEventListener(
+  "mouseenter",
+  (event) => {
+    if (event.target.classList.contains("square")) {
+      event.target.style.backgroundColor = generateRandomRGBColor();
+    }
+  },
+  true
+);
+
+function generateRandomRGBColor() {
+  return `rgb(
+    ${Math.floor(Math.random() * 256)}, 
+    ${Math.floor(Math.random() * 256)}, 
+    ${Math.floor(Math.random() * 256)})`;
+}
+
+// create and style squares on grid
+function createGridSquare(gridSize) {
   const square = document.createElement("div");
   square.classList.add("square");
-  square.style.flex = `1 0 ${100 / defaultGridSize}%`; // gives each square an even space on the grid
+  square.style.flex = `1 0 ${100 / gridSize}%`; // gives each square an even space on the grid
   square.style.boxSizing = "border-box";
   square.style.border = "1px solid #ccc";
-  grid.appendChild(square);
-  // on hover fill the square with a random rgb color
-  square.addEventListener(
-    "mouseenter",
-    (event) => {
-      event.target.style.backgroundColor = getRandomColor();
-    },
-    false
-  );
+  gridContainer.appendChild(square);
+  return square;
 }
 
-function getRandomColor() {
-  return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
-    Math.random() * 256
-  )}, ${Math.floor(Math.random() * 256)})`;
-}
+gridSizeButton.addEventListener("click", () => {
+  const size = parseInt(prompt("Enter a grid size (max 100):"));
+  if (size && size > 0 && size <= 100) {
+    userGridSize = size;
+    renderGrid();
+  } else {
+    alert("Please enter a valid number between 1 and 100.");
+  }
+});
